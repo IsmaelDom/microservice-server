215f50e360cf133b249ba2b1eb14c9fbbcc90435
Ismael Domínguez Martínez <ismaeldom11@gmail.com>
2022-10-28 11:30:06 -0500

    Commit inicial.

---
+++ src/test/java/com/eureka/naming/server/microserviceserver/MicroserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.naming.server.microserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
